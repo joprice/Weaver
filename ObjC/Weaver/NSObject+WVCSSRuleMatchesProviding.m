8982a5ffb380f63d855eb015122540a4d722ea49
Joseph Price <pricejosephd@gmail.com>
2021-01-23 12:11:41 -0500

    remove attributed fields

--- ObjC/Weaver/NSObject+WVCSSRuleMatchesProviding.m
+++ ObjC/Weaver/NSObject+WVCSSRuleMatchesProviding.m
@@ -322,6 +322,7 @@ ASDISPLAYNODE_INLINE AS_WARN_UNUSED_RESULT NSString *NSHexStringFromColor(UIColo
 
 @end
 
+/*
 @implementation ASTextNode2 (PDCSSRuleMatchesProviding)
 
 - (NSArray<PDCSSProperty *> *)wv_CSSProperties
@@ -344,6 +345,7 @@ ASDISPLAYNODE_INLINE AS_WARN_UNUSED_RESULT NSString *NSHexStringFromColor(UIColo
 }
 
 @end
+*/
 
 @implementation ASTextNode (PDCSSRuleMatchesProviding)
 
@@ -351,8 +353,9 @@ ASDISPLAYNODE_INLINE AS_WARN_UNUSED_RESULT NSString *NSHexStringFromColor(UIColo
 {
   NSMutableArray<PDCSSProperty *> *result = [NSMutableArray arrayWithArray:[super wv_CSSProperties]];
   [result addObject:[PDCSSProperty propertyWithName:@"attributedText" value:self.attributedText.string]];
-  [result addObject:[PDCSSProperty propertyWithName:@"truncationAttributedText" value:self.truncationAttributedText.string]];
-  [result addObject:[PDCSSProperty propertyWithName:@"additionalTruncationMessage" value:self.additionalTruncationMessage.string]];
+  //TODO: these are missing "value" field in json
+  //[result addObject:[PDCSSProperty propertyWithName:@"truncationAttributedText" value:self.truncationAttributedText.string]];
+  //[result addObject:[PDCSSProperty propertyWithName:@"additionalTruncationMessage" value:self.additionalTruncationMessage.string]];
   [result addObject:[PDCSSProperty propertyWithName:@"truncationMode" value:@(self.truncationMode).stringValue]]; // Enum
   [result addObject:[PDCSSProperty propertyWithName:@"truncated" value:(self.truncated ? @"YES" : @"NO")]];
   [result addObject:[PDCSSProperty propertyWithName:@"maximumNumberOfLines" value:@(self.maximumNumberOfLines).stringValue]];
